5c853a4d0e8a2ca783a402d1712e6c799bad658f
kyle rodgers <kyle77r@gmail.com>
2019-12-09 23:17:33 -0800

    Added tests to test the game functions

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -18,3 +18,105 @@ TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
+
+TEST(PiezasTest, resetTest) {
+	Piezas Game;
+	Game.dropPiece(2);
+	Game.reset();
+	ASSERT_EQ(Game.pieceAt(0,2), Blank);
+}
+
+TEST(PiezasTest, dropPieceBounds) {
+	Piezas Game;
+	ASSERT_EQ(Game.dropPiece(5), Invalid);
+}
+
+TEST(PiezasTest, dropPieceTurnLogic) {
+	Piezas Game;
+	Game.dropPiece(2);
+	Game.dropPiece(2);
+	ASSERT_EQ(Game.dropPiece(2), X);
+}
+
+TEST(PiezasTest, dropPieceOverflow) {
+	Piezas Game;
+	Game.dropPiece(2);
+	Game.dropPiece(2);
+	Game.dropPiece(2);
+	ASSERT_EQ(Game.dropPiece(2), Blank);
+}
+
+TEST(PiezasTest, pieceAtBounds) {
+	Piezas Game;
+	ASSERT_EQ(Game.pieceAt(0,5), Invalid);
+}
+
+TEST(PiezasTest, pieceAtTile) {
+	Piezas Game;
+	Game.dropPiece(2);
+	ASSERT_EQ(Game.pieceAt(0,2), X);
+}
+
+TEST(PiezasTest, pieceAtBlank) {
+	Piezas Game;
+	ASSERT_EQ(Game.pieceAt(0,2), Blank);
+}
+
+TEST(PiezasTest, gameStateNotDone) {
+	Piezas Game;
+	Game.dropPiece(0); //X
+	Game.dropPiece(1); //O
+	ASSERT_EQ(Game.gameState(), Invalid);
+}
+
+TEST(PiezasTest, gameStateXWin) {
+	Piezas Game;
+	Game.dropPiece(0); //X
+	Game.dropPiece(1); //O
+	Game.dropPiece(0); //X
+	Game.dropPiece(2); //O
+	Game.dropPiece(0); //X
+	Game.dropPiece(3); //O
+	Game.dropPiece(1); //X
+	Game.dropPiece(1); //O
+	Game.dropPiece(3); //X
+	Game.dropPiece(2); //O
+	Game.dropPiece(2); //X
+	Game.dropPiece(3); //O
+	ASSERT_EQ(Game.gameState(), X);
+}
+
+TEST(PiezasTest, gameStateOWin) {
+	Piezas Game;
+	Game.dropPiece(1); //X
+	Game.dropPiece(0); //O
+	Game.dropPiece(2); //X
+	Game.dropPiece(0); //O
+	Game.dropPiece(3); //X
+	Game.dropPiece(0); //O
+	Game.dropPiece(1); //X
+	Game.dropPiece(1); //O
+	Game.dropPiece(3); //X
+	Game.dropPiece(2); //O
+	Game.dropPiece(2); //X
+	Game.dropPiece(3); //O
+	ASSERT_EQ(Game.gameState(), O);
+}
+
+TEST(PiezasTest, gameStateTie) {
+	Piezas Game;
+	Game.dropPiece(1); //X
+	Game.dropPiece(0); //O
+	Game.dropPiece(0); //X
+	Game.dropPiece(0); //O
+	Game.dropPiece(3); //X
+	Game.dropPiece(1); //O
+	Game.dropPiece(1); //X
+	Game.dropPiece(2); //O
+	Game.dropPiece(2); //X
+	Game.dropPiece(2); //O
+	Game.dropPiece(2); //X
+	Game.dropPiece(3); //O
+	Game.dropPiece(3); //X
+	ASSERT_EQ(Game.gameState(), Blank);
+}
